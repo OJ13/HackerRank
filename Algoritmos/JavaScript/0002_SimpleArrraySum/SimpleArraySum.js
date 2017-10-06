c07fbdd8033a2455a5a3c76528201099ec1fa576
Desenvolvimento <Desenvolvimento@grupogtp.br>
2017-10-06 09:04:58 -0300

    algoritmos exe2

--- Algoritmos/JavaScript/0002_SimpleArrraySum/SimpleArraySum.js
+++ Algoritmos/JavaScript/0002_SimpleArrraySum/SimpleArraySum.js
@@ -1,10 +1,11 @@
 console.log("Simple Array Sum");
 
+var inp = [];
+
 function qtdNumbers(a){
 
     for(i = 0; i < a ; i++){
         console.log(i);
-        var inp = [];
         inp[i] = document.createElement("input");
         inp[i].type = "number"
         inp[i].id = "inp" + i;
@@ -27,7 +28,11 @@ function gerar(){
 
 function simpleArraySum(n, ar) {
     // Complete this function
-
+    var result = 0;
+    for(i = 0; i < n; i++){
+        result = result + ar[i];
+    }
+    return result;
 }
 
 function main() {
@@ -37,4 +42,22 @@ function main() {
     // ar = ar.map(Number);
     // var result = simpleArraySum(n, ar);
     // process.stdout.write("" + result + "\n");
+    if(!inp){
+        console.log(inp);
+        document.getElementById('lblResultado').innerText = "Insira valores nas caixas"
+    }else{
+        console.log(inp);
+        var result = simpleArraySum2(inp);
+        console.log(result);
+        document.getElementById('lblResultado').innerText = result;
+    }
+}
+
+function simpleArraySum2(ar){
+
+    var resultado = 0;
+    for(var i = 0; i < ar; i++){
+        
+        resultado = resultado + ar[i].value;
+    }    
 }
